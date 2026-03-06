839cf89c9a6fbddb7ed300f7bf1c073372022d00
Fernando Camargo <fcneves81@gmail.com>
2026-03-05 22:01:33 -0300

    removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     # return HTTP response
-    return HttpResponse('Olá, Mundo! Meu site com Django!')
-
-# HTTP REQUEST
-def contato(request):
-    # return HTTP response
-    return HttpResponse('CONTATO')
-
-# HTTP REQUEST
-def sobre(request):
-    # return HTTP response
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/home.html', context={
+        'name' : 'Fernando'
+    })
